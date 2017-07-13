7c1c0a3f77abe19b8d5548f59eaf751821a9a2c3
anthonyASC4 <aemaldonado@icloud.com>
2017-07-13 01:26:55 -0400

    Add function practice and rand() function

--- anthony/anthony.js
+++ anthony/anthony.js
@@ -1,5 +1,21 @@
-console.log("Anthony is awesome");
-console.log("i like pancakes");
-console.log("i");
-console.log("like");
-console.log("pancakes");
+// TASK
+// Create a rand() function
+// takes a number
+// Returnsa random number betwen 0 and that number
+
+// PSEUDOCODE
+// Multiply Math.random() by the given number
+// remove the decimal part
+// return the remaining integer
+
+function rand (num) {
+    // returns a random integer between 0 and num 
+    var randomNum = num * Math.random();
+    var result = Math.floor(randomNum);
+    return result;
+}
+
+console.log(rand(6));
+console.log(rand(1000));
+console.log(rand(2));
+console.log(rand(0));
---
+++ anthony/functions.js
@@ -0,0 +1,10 @@
+// function welloHorld (){
+//     console.log("Bye");
+// return "Wello, Horld";
+// }
+// welloHorld();
+
+function hello(name){
+console.log ("Hello, " + name + "!");
+}
+hello("Anthony");
